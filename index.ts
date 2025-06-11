c78fde00f921fa2565a4a715ba79a4be7557a17d
Bhuban <bhubanpadun15m37@gmail.com>
2025-06-11 19:57:10 +0530

    change config

--- index.ts
+++ index.ts
@@ -1,12 +1,24 @@
-import {Router,Request} from "express"
+import express,{Router,Request} from "express"
 import AddProduct from "./src/routes/v1/postProperty"
 import ManageProduct from "./src/routes/v1/vendor"
 import { CheckPrivillage } from "./src/middleware/apiAccessPermissionCheck"
+import bodyParser from "body-parser"
+import cors from "cors"
 
 
 const route = Router()
-route.use('/product',AddProduct)
-route.use("/manage",ManageProduct)
+const app = express();
+const PORT = process.env.PORT || 8080
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api/product', AddProduct)
+app.use('/api/manage', ManageProduct)
+app.get('/', (req, res) => {
+    res.send('Server is running...')
+});
 
 
-export default route
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
