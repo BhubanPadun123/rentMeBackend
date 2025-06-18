c3b10307bcd9804f323ba782bf400b7499cb8bc8
Bhuban <bhubanpadun15m37@gmail.com>
2025-06-18 12:20:33 +0530

    add version

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import express,{Router,Request} from "express"
+import express, { Router, Request } from "express"
 import AddProduct from "./src/routes/v1/postProperty"
 import ManageProduct from "./src/routes/v1/vendor"
 import { CheckPrivillage } from "./src/middleware/apiAccessPermissionCheck"
 import bodyParser from "body-parser"
 import cors from "cors"
 import auth from "./src/routes/auth/auth.controller"
-import {verifyToken} from "./src/middleware/auth.middleware"
+import { verifyToken } from "./src/middleware/auth.middleware"
 import connectDB from "./src/config/db"
 // import Rconnect from "./src/config/radisConnect"
 import CustomerManage from "./src/routes/v1/customer"
@@ -19,14 +19,20 @@ const PORT = process.env.PORT || 8080
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use('/api/product',verifyToken, AddProduct)
-app.use('/api/manage',verifyToken, ManageProduct)
-app.use("/api/customer",verifyToken,CustomerManage)
-app.use('/api/auth',auth)
+app.use('/api/product', verifyToken, AddProduct)
+app.use('/api/manage', verifyToken, ManageProduct)
+app.use("/api/customer", verifyToken, CustomerManage)
+app.use('/api/auth', auth)
 app.get('/', (req, res) => {
     res.send('Server is running...')
 });
-(async function(){
+app.get('/version', (req, res) => {
+    res.json({
+        version: 'v1.0.3',
+        deployedAt: '2025-06-18 11:57',
+    });
+});
+(async function () {
     await connectDB()
     // await Rconnect()
 })()
